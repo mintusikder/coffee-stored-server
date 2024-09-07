1fd69b1f499a8a8033854e71311b30a3486370bb
Mintu Sikder <mintusikder15@gmail.com>
2024-09-07 22:52:45 +0600

    added delete oppration

--- index.js
+++ index.js
@@ -89,6 +89,12 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
